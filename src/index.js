bb71793c0f493596c9d5950da4ae11c3c4249ab4
VanWilder199 <72692962+VanWilder199@users.noreply.github.com>
2021-01-14 19:48:58 +0300

    init: add
    complete Task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+const { compare } = require("semver");
 
 exports.min = function min (array) {
-  return 0;
+   if (array == undefined || array.length === 0) return 0;
+    
+   let x = array.sort((a, b) => a - b);
+    return x[0]
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == undefined || array.length === 0) return 0;
+    let largest = Math.max(...array)
+    return largest
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == undefined || array.length === 0) return 0;
+  let sum = 0;
+  for (let i = 0; i < array.length; i++){
+      sum += array[i]
+  }
+   let avgA = sum / array.length; 
+   return avgA;
 }
